f932fc76f6716d1341f373995a9d986113dce7cb
Vivi Poit <vwpoit@gmail.com>
2024-03-31 18:36:34 -0400

    change style based on flake8 linting

--- calculator.py
+++ calculator.py
@@ -1,8 +1,10 @@
 def add(first_item, second_item):
-	return first_item + second_item
+    return first_item + second_item
+
 
 def subtract(first_item, second_item):
-	return first_item - second_item
+    return first_item - second_item
+
 
 def multiply(first_item, second_item):
-	return first_item * second_item
+    return first_item * second_item
